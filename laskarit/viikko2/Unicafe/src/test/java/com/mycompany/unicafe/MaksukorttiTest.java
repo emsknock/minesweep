73489cf054e838835b42a3ec8b296d51a0d64096
emsknock <25252659+emsknock@users.noreply.github.com>
2020-11-02 20:15:51 +0200

    Add required tests

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,49 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+
+    // kortin saldo alussa oikein
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertTrue(kortti.saldo() == 10);      
+    }
+
+    // rahan lataaminen kasvattaa saldoa oikein
+    @Test
+    public void lataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertTrue(kortti.saldo() == 20);      
+    }
+
+    // rahan ottaminen toimii
+    @Test
+    public void rahanOttaminenToimii() {
+        kortti.otaRahaa(10);
+        assertTrue(kortti.saldo() == 0);      
+    }
+
+    // saldo vähenee oikein, jos rahaa on tarpeeksi
+    @Test
+    public void saldoVäheneeJosOnRahaa() {
+        kortti.otaRahaa(5);
+        assertTrue(kortti.saldo() == 5);      
+    }
+
+    // saldo ei muutu, jos rahaa ei ole tarpeeksi
+    @Test
+    public void saldoEiMuutuJosEiRahaa() {
+        kortti.otaRahaa(20);
+        assertTrue(kortti.saldo() == 10);      
+    }
+
+    // otaRahaa palauttaa true, jos rahat riittivät ja muuten false
+    @Test
+    public void otaRahaaPalauttaaTrueJosRahaRiittää() {
+        assertTrue(kortti.otaRahaa(10));      
+    }
+    @Test
+    public void otaRahaaPalauttaaFalseJosRahaEiRiitä() {
+        assertFalse(kortti.otaRahaa(20));      
+    }
+
 }
