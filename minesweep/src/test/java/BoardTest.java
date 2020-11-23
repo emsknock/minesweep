415c7ebfd2e720dbc33c45e9051174bb467cae90
emsknock <25252659+emsknock@users.noreply.github.com>
2020-11-23 12:48:10 +0200

    Create mine count test

--- minesweep/src/test/java/BoardTest.java
+++ minesweep/src/test/java/BoardTest.java
@@ -15,4 +15,29 @@ public class BoardTest {
         }
     }
 
+
+    private int getMineCount(Board b) {
+        Square[][] grid = b.getGrid();
+        int mineCount = 0;
+        for (Square[] row : grid) {
+            for (Square col : row) {
+                mineCount += col.isMine ? 1 : 0;
+            }
+        }
+        return mineCount;
+    }
+    @Test
+    public void createdBoardHasCorrectNumOfMines() {
+        Board[] _b = {
+            new Board(256, 128, 8, 1L),
+            new Board(256, 128, 4, 1L),
+            new Board(256, 128, 2, 1L),
+            new Board(256, 128, 1, 1L),
+        };
+        assertEquals(8, getMineCount(_b[0]));
+        assertEquals(4, getMineCount(_b[1]));
+        assertEquals(2, getMineCount(_b[2]));
+        assertEquals(1, getMineCount(_b[3]));
+    }
+
 }
