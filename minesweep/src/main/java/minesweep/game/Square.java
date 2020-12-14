502f5204f0f593e51aae6c168f7668efda6b7033
emsknock <25252659+emsknock@users.noreply.github.com>
2020-12-14 20:14:45 +0200

    Square de/serialisation

--- minesweep/src/main/java/minesweep/game/Square.java
+++ minesweep/src/main/java/minesweep/game/Square.java
@@ -9,9 +9,27 @@ public class Square {
 
     public int y = 0;
     public int x = 0;
+
     public Square(int y, int x) {
         this.y = y;
         this.x = x;
+    }
+
+    public byte serialise() {
+        return (byte) (
+            (isMine ? 1 : 0) << 4 |
+            (isFlagged ? 1 : 0) << 5 |
+            (isRevealed ? 1 : 0) << 6 |
+            mineNeighbours
+        );
+    }
+
+    public static Square deserialise(byte serialised, int y, int x) {
+        Square out = new Square(y, x);
+        out.isMine = ((serialised >> 4) & 1) == 1;
+        out.isFlagged = ((serialised >> 5) & 1) == 1;
+        out.isRevealed = ((serialised >> 6) & 1) == 1;
+        return out;
     }
 
 }
